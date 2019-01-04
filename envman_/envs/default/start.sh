d1061df1e06ed6a237b12514f87feb60e25ef36c
Bluepython 508 <bluepython508@gmail.com>
2019-01-04 14:47:32 +0000

    Fix included files

--- envman_/envs/default/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Environment: default
-# Environment path: /Users/ben/PycharmProjects/EnvManager/envman_/envs/default
-# $ENV is the environment's name and $ENV_PATH is the environment's path
--- envman_/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This file is executed every time a new environment is entered. $ENV is the environment's name and $ENV_PATH is the environment's path
-export ENVMAN_OLDPS1="$PS1" # Please do not remove this unless you know what you are doing
-export PS1="[$ENV] $PS1"
